65729b3a15b08c5a90d7c2566e2b0e65a394753e
M.Ajwad <72156399+DeCryptAF@users.noreply.github.com>
2024-05-18 14:06:00 +0600

    5.odd_even_array.c

--- 5.odd_even_array.c
+++ 5.odd_even_array.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 
 int main() {
-  int arr[] = {1, 2, 3, 4, 5};
-  int size = sizeof(arr) / sizeof(arr[0]);
-  int odd[size], even[size];
-  int odd_size = 0, even_size = 0;
-  
-  for (int i = 0; i < size; i++)
-  {
-    if (arr[i] % 2 == 0) even[even_size] = arr[i], even_size++;
-    else odd[odd_size] = arr[i], odd_size++;
-  }
-
-  printf("Original array: ");
-  for (int i = 0; i < size; i++) 
-    printf("%d ", arr[i]);
-  printf("\n");
-  printf("Odd elements: ");
-  for (int i = 0; i < odd_size; i++)
-    printf("%d ", odd[i]);
-  printf("\n");
-  printf("Even elements: ");
-  for (int i = 0; i < even_size; i++)
-    printf("%d ", even[i]);
-  printf("\n");
-
-  return 0;
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int odd[size], even[size];
+    int odd_size = 0, even_size = 0;
+    
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] % 2 == 0) even[even_size] = arr[i], even_size++;
+        else odd[odd_size] = arr[i], odd_size++;
+    }
+    
+    printf("Original array: ");
+    for (int i = 0; i < size; i++) 
+        printf("%d ", arr[i]);
+    printf("\n");
+    printf("Odd elements: ");
+    for (int i = 0; i < odd_size; i++)
+        printf("%d ", odd[i]);
+    printf("\n");
+    printf("Even elements: ");
+    for (int i = 0; i < even_size; i++)
+        printf("%d ", even[i]);
+    printf("\n");
+    
+    return 0;
 }
