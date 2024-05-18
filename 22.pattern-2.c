d602a8c125042b0ce2e90db7d36fbda811be49c7
M.Ajwad <72156399+DeCryptAF@users.noreply.github.com>
2024-05-18 11:24:41 +0600

    22.pattern-2.c

---
+++ 22.pattern-2.c
@@ -0,0 +1,35 @@
+//         *
+//      *     *
+//    *         *
+//      *     *
+//         *
+
+#include <stdio.h>
+
+int main() {
+    int n = 5;
+    for(int i = 1; i <=n; i++) {
+        for(int space = 1; space <= (n - i); space++) {
+            printf(" ");
+        }
+        for(int j = 1; j <= i; j++) {
+            if(j == 1 || j == i) {
+                printf(" * ");
+            } else printf("  ");
+        }
+        printf("\n");
+    }
+
+    for(int i = n - 1; i > 0; i--) {
+        for(int space = 1; space <= (n - i); space++) {
+            printf(" ");
+        }
+        for(int j = 1; j <= i; j++) {
+            if(j == 1 || j == i) {
+                printf(" * ");
+            } else printf("  ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
