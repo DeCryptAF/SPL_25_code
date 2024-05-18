ab9b29eaa64c0eaf825809591bdccb46566fc8d5
M.Ajwad <72156399+DeCryptAF@users.noreply.github.com>
2024-05-18 13:10:29 +0600

    21.pattern-1.c

--- 21.pattern-1.c
+++ 21.pattern-1.c
@@ -1,24 +1,24 @@
 //    1
-//   2 2
-//  3 3 3
-// 4 4 4 4
+//  2 1 2
+//3 2 1 2 3
+
 
 #include <stdio.h>
 
 int main() {
-    int n;
-    printf("Enter the number of rows: ");
-    scanf("%d", &n);
-
+    int n = 3;
     for(int i = 1; i <=n; i++) {
-        for(int space = 1; space <= (n - i); space++) {
+        for (int j = 0; j < n - i; j++) {
             printf("  ");
         }
-        for(int j = 1; j <= i; j++) {
-            printf("%4d", i);
+        for (int j = i; j >= 1; j--) {
+            printf("%d ", j);
+        }
+        for (int j = 2; j <= i; j++) {
+            printf("%d ", j);
         }
         printf("\n");
     }
-    
     return 0;
 }
+
